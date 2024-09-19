0841f03dd6d141fa06be9ef2a3bc9ef30ea39248
ImanArs <imanbaevemaill.ru@gmail.com>
2024-09-19 19:49:18 +0600

    add styles files

--- src/components/Checkbox/Checkbox.svelte
+++ src/components/Checkbox/Checkbox.svelte
@@ -30,44 +30,5 @@
 </div>
 
 <style>
-  .checkboxWrapper {
-    display: flex;
-    align-items: center;
-  }
-
-  input[type="checkbox"] {
-    display: none;
-  }
-
-  .checkboxIcon {
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    border: 2px solid #3F4363;
-    border-radius: 4px;
-    margin-right: 8px;
-    position: relative;
-  }
-
-  .checkboxIcon img {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 16px;
-    height: 16px;
-    color: #fff;
-  }
-
-  input[type="checkbox"]:checked + label .checkboxIcon {
-    background-color: #3F4363;
-  }
-
-  label {
-    cursor: pointer;
-    font-size: 18px;
-    font-weight: 400;
-    color: #fff;
-    margin-top: 6px;
-  }
+  @import './Checkbox.css'
 </style>
